4edc78a94eec7e2c62e9b8d53a43f07ceb0787c8
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-04-12 05:53:29 -0400

    Converted vehicle type into class and created public getters and setters.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,51 @@
 import { v4 as uuidv4 } from 'uuid';
 
-type vehicle = {
-    id: string;
-    name: string;
-    price: number;
-    description: string;
-};
 
+
+
+//Vehicle Start
+class Vehicle {
+    private _id: string;
+    private _name: string;
+    private _price: number;
+    private _description: string;
+
+    constructor(name: string, price: number, description: string) {
+        this._id = uuidv4();
+        this._name = name;
+        this._price = price;
+        this._description = description;
+    }
+
+    get id(): string {
+        return this._id;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    set name(value: string) {
+        this._name = value;
+    }
+
+    get price(): number {
+        return this._price;
+    }
+
+    set price(value: number) {
+        this._price = value;
+    }
+
+    get description(): string {
+        return this._description;
+    }
+
+    set description(value: string) {
+        this._description = value;
+    }
+}
+//Vehicle end
 
 type user = {
     id: string;
